89535797e3942498714754c1c5b56c1d0035f760
SaKaarle <saku.kaarlejarvi@gmail.com>
2020-11-19 20:09:39 +0200

    toimiva react route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useRef, useState } from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Grid from './grid';
+import Home from './home';
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <Link to="/">Home</Link> <Link to="/contact">Contact</Link>{" "}
+          <Link to="/links">Links</Link>{" "}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/contact" component={Grid} />
+            <Route path="/links" render={() => <h1>Links</h1>} />
+            <Route render={() => <h1>Page not found</h1>} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/grid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Grid = () => {
+    return(
+        <div>
+            <h1>
+                grid.js)))
+            </h1>
+
+        </div>
+    )
+}
+
+export default Grid
